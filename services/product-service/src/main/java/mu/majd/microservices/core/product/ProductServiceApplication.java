677e4c4d7461db672308f894edfd8ecec6213237
mmajd <mabulmagd55@gmail.com>
2022-05-22 21:02:05 +0200

    add: some services & properties files

--- services/product-service/src/main/java/mu/majd/microservices/core/product/ProductServiceApplication.java
+++ services/product-service/src/main/java/mu/majd/microservices/core/product/ProductServiceApplication.java
@@ -2,8 +2,13 @@ package mu.majd.microservices.core.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+
 
 @SpringBootApplication
+@ComponentScan("mu.majd")
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
