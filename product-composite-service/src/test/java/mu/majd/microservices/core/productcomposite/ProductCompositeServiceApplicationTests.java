19d6a551fbfeaeb09855c125bf90063bf7d018ff
mmajd <mabulmagd55@gmail.com>
2022-05-22 17:49:36 +0200

    init commit

---
+++ product-composite-service/src/test/java/mu/majd/microservices/core/productcomposite/ProductCompositeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mu.majd.microservices.core.productcomposite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCompositeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
