3b8b265bc1e6e5f4943a697f95a664d31de2ac80
mmajd <mabulmagd55@gmail.com>
2022-06-26 10:33:35 +0200

    Chage: restructure

---
+++ microservices/product-service/src/main/java/mmajd/microservices/core/product/services/ProductServiceImpl.java
@@ -0,0 +1,39 @@
+package mmajd.microservices.core.product.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import mmajd.api.core.product.Product;
+import mmajd.api.core.product.ProductService;
+import mmajd.api.exceptions.InvalidInputException;
+import mmajd.api.exceptions.NotFoundException;
+import mmajd.util.http.ServiceUtil;
+
+@RestController
+public class ProductServiceImpl implements ProductService {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
+
+  private final ServiceUtil serviceUtil;
+
+  @Autowired
+  public ProductServiceImpl(ServiceUtil serviceUtil) {
+    this.serviceUtil = serviceUtil;
+  }
+
+  @Override
+  public Product getProduct(int productId) {
+    LOG.debug("/product return the found product for productId={}", productId);
+
+    if (productId < 1) {
+      throw new InvalidInputException("Invalid productId: " + productId);
+    }
+
+    if (productId == 13) {
+      throw new NotFoundException("No product found for productId: " + productId);
+    }
+
+    return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
+  }
+}
